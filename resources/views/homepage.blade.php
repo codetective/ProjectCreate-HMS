0d9df6d477da271f1ddff54791b01b4bc57fae7a
Ememekong001 <airmyekong@gmail.com>
2020-09-24 15:49:27 +0100

    second commit which includes the addition of the login buttons

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,8 +4,26 @@
 
 <section id="bacground">
     <div>
-        
+        <div id="central" >
+            <h1>Alpha Hospital <br> Management System</h1>
+        </div>
 
+    </div>
+    <div>
+       <div class="row"id="centra">
+        <div class="card" style="width: 15rem;">
+            <div class="card-body">
+                <span id="icon"><i class="fa fa-github" aria-hidden="true"></i></i></span>
+                <a href="#" class="btn btn-primary">Login as an Admin</a>
+            </div>
+          </div>
+          <div class="card" style="width: 15rem;">
+            <div class="card-body">
+                <span id="icon"><i class="fa fa-heartbeat" aria-hidden="true"></i></i></span>
+              <a href="#" class="btn btn-primary">Login as an Employee</a>
+            </div>
+          </div>
+       </div>
     </div>
 </section>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>HMS</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playball">
         <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     </head>
     <body>
-        <header>
-            <!-- Image and text -->
-            <nav class="navbar navbar-light bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                        <img src="{{ asset('img/logo.png') }}" width="60" height="40" style="border-radius:40%" class="d-inline-block align-top" alt="" loading="lazy"> Alpha
-                    </a>
-                </div>
-            </nav>
-        </header>
-
+    
      <div class="container-fluid">
         @yield('content')
      </div>
