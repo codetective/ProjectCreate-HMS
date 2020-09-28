01a528d373d50dea37fe201abc7584237a4eb524
codetective <iaminiodu@gmail.com>
2020-09-28 06:43:43 +0100

    updated readme

--- storage/framework/views/a2253032a4a71daf62a653af17ce997618b2d7e2.php
+++ storage/framework/views/a2253032a4a71daf62a653af17ce997618b2d7e2.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Alpha HMS</title>
-    <link rel="stylesheet" href=<?php echo e(asset('css/bootstrap.min.css')); ?>>
+    <link rel="stylesheet" href=<?php echo e(asset('css/bootswatch.css')); ?>>
     <!-- Latest compiled and minified CSS -->
 </head>
 
